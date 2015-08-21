067a5701c866d4431113e3b14ed39b8d77119e43
ThePersister <Ahm_Brouwers@hotmail.com>
2015-08-21 22:43:43 +0200

    LevelEditorSetup :+1:

---
+++ Assets/Editor/RoadDrawer.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(IDrawable))]
+public class RoadDrawer : Editor {
+
+    private bool isFiring;
+    private IDrawable m_Target;
+
+    public override void OnInspectorGUI()
+    {
+        m_Target = (IDrawable)target;
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button((m_Target.IsAddingOrDeleting) ? "Adding, toggle to delete blocks" : "Deleting, toggle to add blocks"))
+        {
+            m_Target.IsAddingOrDeleting = !m_Target.IsAddingOrDeleting;
+        }
+
+        GUILayout.Space(10f);
+        GUI.color = Color.red;
+
+        if (GUILayout.Button("Clear Blocks"))
+        {
+            IBlock[] blocks = (IBlock[])FindObjectsOfType(typeof(IBlock));
+            foreach (IBlock block in blocks)
+            {
+                DestroyImmediate(block.gameObject);
+            }
+            EditorUtility.SetDirty(m_Target);
+        }
+
+        GUI.color = Color.white;
+    }
+
+    void OnSceneGUI()
+    {
+        if (Event.current.type == EventType.MouseDown)
+        {
+            GatherPoints();
+            isFiring = !isFiring;
+        }
+        else if (Event.current.type == EventType.MouseMove)
+        {
+            GatherPoints();
+        }
+    }
+
+    private void GatherPoints()
+    {
+        if (!isFiring) return;
+        //isFiring = false; // Disable for continuous drawing
+
+        Vector3 viewportPoint = Camera.current.ScreenToViewportPoint(Event.current.mousePosition);
+        //Debug.Log(viewportPoint);
+        viewportPoint = new Vector3(viewportPoint.x, 1 - viewportPoint.y, viewportPoint.z); // Invert the Y
+        //Debug.Log(viewportPoint);
+
+        Ray ray = Camera.current.ViewportPointToRay(viewportPoint);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+        if (hit.collider != null)
+            m_Target.DrawAction(hit);
+
+        /*
+        //Debug.DrawRay(ray.origin, ray.direction * 100000000f, Color.yellow, 5f);
+        if (Physics.Raycast(ray, out hit, 100000.0f))
+        {
+            m_Target.DrawAction(hit);
+            //Debug.Log("Yay3");
+
+            EditorUtility.SetDirty(m_Target);
+
+
+            //Debug.Log(Event.current.mousePosition);
+            //Debug.DrawLine(Camera.current.transform.position, hit.point, Color.yellow, 30f);
+            
+            //m_Target.RoadTest.Add(hit.point);
+        }*/
+    }
+}
---
+++ Assets/_Main/Scripts/IDrawable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System.Collections;
+using System.Collections.Generic;
+
+[SelectionBase]
+public class IDrawable : MonoBehaviour {
+
+    public bool IsAddingOrDeleting = true;
+    public float RoundFactor = 50f;
+    public LayerMask OnlyDrawable;
+    public LayerMask OnlyBlock;
+    public GameObject PFB_Block;
+    public Transform LevelOverseer;
+
+    public void DrawAction(RaycastHit2D hitData)
+    {
+        if (IsAddingOrDeleting && hitData.collider.gameObject.layer == (int)Mathf.Log(OnlyDrawable.value, 2))
+        {
+            Vector2 levelPiece = new Vector2(Mathf.RoundToInt(hitData.point.x / RoundFactor) * RoundFactor, Mathf.RoundToInt(hitData.point.y / RoundFactor) * RoundFactor);
+            GameObject prefab = (GameObject)Instantiate(PFB_Block, levelPiece, Quaternion.identity);
+            prefab.transform.parent = LevelOverseer;
+        }
+        else if (!IsAddingOrDeleting && hitData.collider.gameObject.layer == (int)Mathf.Log(OnlyBlock.value, 2))
+        {
+            DestroyImmediate(hitData.collider.gameObject);
+        }
+    }
+
+    void OnDrawGizmos()
+    {/*
+        foreach (Vector3 roadPoint in RoadEdgePoints)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(new Vector3(roadPoint.x, 0, roadPoint.y), 2f);
+            //Handles.PositionHandle(new Vector3(roadPoint.x, 0, roadPoint.y), Quaternion.identity);
+            //Gizmos.DrawSphere(roadPoint, 3f);
+        }*/
+    }
+}
